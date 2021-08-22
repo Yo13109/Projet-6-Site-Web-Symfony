070d7eabaa804658de7e2edd4c36f51cd11e7e38
Yoann Corsi <yoann.corsi@gmail.com>
2021-08-22 15:14:41 +0200

    create of relation

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TrickRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -47,6 +49,28 @@ class Trick
      * @ORM\JoinColumn(nullable=false)
      */
     private $category;
+
+    /**
+     * @ORM\OneToMany(targetEntity=video::class, mappedBy="trick")
+     */
+    private $video;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Picture::class, mappedBy="trick")
+     */
+    private $picture;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Commentary::class, mappedBy="trick")
+     */
+    private $commentary;
+
+    public function __construct()
+    {
+        $this->video = new ArrayCollection();
+        $this->picture = new ArrayCollection();
+        $this->commentary = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -121,6 +145,96 @@ class Trick
     public function setCategory(?Category $category): self
     {
         $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|video[]
+     */
+    public function getVideo(): Collection
+    {
+        return $this->video;
+    }
+
+    public function addVideo(video $video): self
+    {
+        if (!$this->video->contains($video)) {
+            $this->video[] = $video;
+            $video->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVideo(video $video): self
+    {
+        if ($this->video->removeElement($video)) {
+            // set the owning side to null (unless already changed)
+            if ($video->getTrick() === $this) {
+                $video->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Picture[]
+     */
+    public function getPicture(): Collection
+    {
+        return $this->picture;
+    }
+
+    public function addPicture(Picture $picture): self
+    {
+        if (!$this->picture->contains($picture)) {
+            $this->picture[] = $picture;
+            $picture->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removePicture(Picture $picture): self
+    {
+        if ($this->picture->removeElement($picture)) {
+            // set the owning side to null (unless already changed)
+            if ($picture->getTrick() === $this) {
+                $picture->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Commentary[]
+     */
+    public function getCommentary(): Collection
+    {
+        return $this->commentary;
+    }
+
+    public function addCommentary(Commentary $commentary): self
+    {
+        if (!$this->commentary->contains($commentary)) {
+            $this->commentary[] = $commentary;
+            $commentary->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCommentary(Commentary $commentary): self
+    {
+        if ($this->commentary->removeElement($commentary)) {
+            // set the owning side to null (unless already changed)
+            if ($commentary->getTrick() === $this) {
+                $commentary->setTrick(null);
+            }
+        }
 
         return $this;
     }
