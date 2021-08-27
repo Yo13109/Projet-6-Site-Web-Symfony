304a92efcf7b7ed718f2a5d60901b601f620d621
Yoann Corsi <yoann.corsi@gmail.com>
2021-08-27 14:58:08 +0200

    fixture preparation

--- src/DataFixtures/BlogFixtures.php
+++ src/DataFixtures/BlogFixtures.php
@@ -6,6 +6,10 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Trick;
 use App\Entity\Category;
+use App\Entity\User;
+use App\Entity\Commentary;
+use Symfony\Component\Validator\Constraints\DateTime;
+
 
 
 
@@ -14,16 +18,65 @@ class BlogFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        //Creer 3 catégories fakées
+        for ($i = 1, $i <= 3, $i++ ) {
+
+             $category = new Category();
+             $category -> setName($faker->sentence());
+
+        $manager->persist($category);
+
+   //Creer 3 users fakées
+
+   for($k = 1, $k <= 3, $k++ ) {
+       $user = new User();
+       $user->setEmail($faker->freeEmail())
+            ->setAvatar($faker->imageUrl())
+            ->setRoles($faker->sentence())
+            ->setPassword($faker->sentence())
+            ->setToken($faker->sentence())
+            ->setActivated($faker->boolean());
+
+       $manager->persist($user);
+            
+
+   }
+
+//Creer des figures
+
+for ($j = 1, $j <= 10, $j++ ) {
 
         $trick = new Trick();
-        $trick->setName("mute")
-            ->setContent("saisie de la carre frontside de la planche entre les deux pieds avec la main avant")
-            ->setCreateDate(new \DateTime())
-            ->setSlug("mute-1")
-            ->setUpdateDate(new \DateTime())
-            ->setCategory(new Category);
+        $content = '<p>'. join($faker->paragraphs(5),'</p><p>') . '</p>';
+        $trick->setName($faker->sentence())
+            ->setContent($content)
+            ->setCreateDate($faker->dateTimeBetween('-6 days'))
+            ->setSlug($faker->sentence())
+            ->setUpdateDate($faker->dateTimeBetween('-6 days'))
+            ->setCategory($category)
+            ->setUsers($user);
 
         $manager->persist($trick);
+
+        }
+        // Creer des commentaires aux figures
+        for ($l = 1, $l = mt_rand(2,5), $l++ ) {
+            
+            $commentary = new Commentary();
+            $content = '<p>'. join($faker->paragraphs(1),'</p><p>') . '</p>';
+            $days = (new DateTime())->diff($trick->getCreateDate())->$days;
+
+            $commentary->setContent($content)
+                       ->setDate($faker->dateTimeBetween('-'. $days .'days'))
+                       ->setUser($user)
+                       ->setTrick($trick);
+            $manager->persist($commentary);
+        
+    }
+
+        
         $manager->flush();
     }
 }
