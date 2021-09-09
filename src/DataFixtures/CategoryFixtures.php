db4b92b7dfd574814c6ee8147c8d524e08700f32
Yoann Corsi <yoann.corsi@gmail.com>
2021-09-09 15:30:03 +0200

    Category + trick fixtures

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -12,16 +12,39 @@ class CategoryFixtures extends Fixture
     {
         $category = new Category();
         $category
-            ->setName('Photo')
+            ->setName('Grab')
         ;
         $this->addReference('category1',$category);
-        //$this->getReference('category1')
+        
+        
 
-        //getDependancies()
-
-        $manager->persist($category);
+        $manager->persist($category);        
         $manager->flush();
-            
+        
+        $category2 = new Category();
+        $category2
+            ->setName('Flip')
+        ;
+        $this->addReference('category2',$category2);
+        $manager->persist($category2);        
+        $manager->flush();
+
+        $category3 = new Category();
+        $category3
+            ->setName('Rotation')
+        ;
+        $this->addReference('category3',$category3);
+        $manager->persist($category3);        
+        $manager->flush();
+
+        $category4 = new Category();
+        $category4
+            ->setName('Slide')
+        ;
+        $this->addReference('category4',$category4);
+        $manager->persist($category4);        
+        $manager->flush();
         
     }
+    
 }
