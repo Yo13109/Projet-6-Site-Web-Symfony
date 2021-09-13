6fb8d31bd552dcb72e3e2b93d9fac2306941d3bd
Yoann Corsi <yoann.corsi@gmail.com>
2021-09-13 11:01:28 +0200

    home page

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,14 +5,17 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\PasswordHasher;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasher;
 
 class UserFixtures extends Fixture
 {
+
+   
+ 
     public function load(ObjectManager $manager)
     {
         $user = new User();
+       
 
         $user
             ->setAvatar('Photo')
@@ -32,3 +35,4 @@ class UserFixtures extends Fixture
         
     }
 }
+
