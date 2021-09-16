92f9fc1452e94059929e9244e512521cb1d6f12d
Yoann Corsi <yoann.corsi@gmail.com>
2021-09-16 15:48:35 +0200

    data contenu

--- src/DataFixtures/TrickFixtures.php
+++ src/DataFixtures/TrickFixtures.php
@@ -15,7 +15,7 @@ use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 class TrickFixtures extends Fixture implements DependentFixtureInterface
 {
     protected $slugger;
-    
+
     public function __construct(SluggerInterface $slugger)
     {
         $this->slugger = $slugger;
@@ -23,7 +23,7 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager)
     {
-        $trick = new Trick();
+        /*  $trick = new Trick();
        
         $user = $this->getReference('user1');
         $date = new DateTime();
@@ -242,8 +242,76 @@ class TrickFixtures extends Fixture implements DependentFixtureInterface
         //getDependancies()
 
         $manager->persist($trick10);
+        $manager->flush();*/
+
+        $datas = [
+            [
+                'name' => 'Methode Air',
+                'content' => 'Un backside ou un air droit où le patineur attrape la planche sur le bord du talon entre les pieds avec sa main avant et cambre le corps en ramenant la planche vers l_arrière.',
+            ],
+            [
+                'name' => 'Nose Grab',
+                'content' => 'Un Nosegrab est un trick de skateboard qui consiste à saisir la planche avec une main au niveau du nose le tout en l_air en faisant un ollie ... C_est sûrement un des grabs le plus facile.',
+            ],
+            [
+                'name' => 'Double Back Flip',
+                'content' => 'C_est un double retournement en arrière dans le plan qui est perpendiculaire à la pente.',
+            ],
+            [
+                'name' => 'Japan Air',
+                'content' => 'saisie de l_avant de la planche, avec la main avant, du côté de la carre frontside.',
+            ],
+            [
+                'name' => 'FrontSite 360',
+                'content' => '360, trois six pour un tour complet',
+            ],
+            [
+                'name' => 'Backside Air',
+                'content' => 'le trick qui marque le plus ta personnalité',
+            ],
+            [
+                'name' => 'Boardslide',
+                'content' => 'Un slide est dit «board slide » lorsque le rider slide littéralement sur la board.',
+            ],
+            [
+                'name' => '50-50',
+                'content' => 'Un 50-50 consiste simplement à glisser le long d_un élement, le contact entre la board et la cible s_effectuant en l_occurrence- au niveau des deux axes',
+            ],
+            [
+                'name' => 'Front Bluntslide',
+                'content' => 'Un slide où il faut faire passer le pied avant au-dessus du rail en arrivant, avec la board perpendiculaire au rail, et faire 3/4 d_un tour sur le rail.',
+            ],
+            [
+                'name' => 'Tail Grab',
+                'content' => 'saisie de la partie arrière de la planche, avec la main arrière',
+            ],
+        ];
+
+        foreach ($datas as  $trickData) {
+
+            $trick = new Trick();
+            $user = $this->getReference('user1');
+            $date = new DateTime();
+            $category = $this->getReference(random_int(1,4));
+            $trick
+                ->setName($trickData['name'])
+                ->setContent($trickData['content'])
+                ->setCreateDate($date)
+                // ->setUpdateDate($date)
+                ->setSlug($this->slugger->slug($trick->getName()))
+                ->setUsers($user)
+                ->setCategory($category);
+
+
+            $this->addReference('?', $trick);
+            //$this->getReference('user1')
+
+            //getDependancies()
+
+            $manager->persist($trick);
+        }
+
         $manager->flush();
-    
     }
     public function getDependencies()
     {
