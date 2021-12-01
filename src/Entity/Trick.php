17065bee73b5eb9d4180ac08da25a345fbc073f8
Yoann Corsi <yoann.corsi@gmail.com>
2021-12-01 12:03:54 +0100

    create a tricks

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,6 +6,7 @@ use App\Repository\TrickRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 
@@ -64,7 +65,7 @@ class Trick
     private $comments;
 
     /**
-     * @ORM\OneToMany(targetEntity=Picture::class, mappedBy="tricks", cascade = {"remove"})
+     * @ORM\OneToMany(targetEntity=Picture::class, mappedBy="tricks", cascade = {"remove","persist"})
      */
     private $pictures;
 
