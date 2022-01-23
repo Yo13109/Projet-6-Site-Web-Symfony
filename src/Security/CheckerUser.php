ed31e8c7a9e9e2dbfbba804820e6dad741180d6f
Yoann Corsi <yoann.corsi@gmail.com>
2022-01-23 23:49:37 +0100

    login

--- src/Security/CheckerUser.php
+++ src/Security/CheckerUser.php
@@ -3,18 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Services\Mailer;
-use App\Form\RegistrationType;
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 
-class Block extends UserCheckerInterface
+class CheckerUser implements UserCheckerInterface
 {
     public function checkPreAuth(UserInterface $user)
     {
